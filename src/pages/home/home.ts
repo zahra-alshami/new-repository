2f0e979917815f6e13b292f490c9731fabb1be77
zahra-alshami <zahrash.95@outlook.com>
2018-07-23 21:14:48 +0200

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users;
 
+  constructor(private nav: NavController, private auth: AuthServiceProvider, public http:Http) {
+    this.getUsers();
+  }
+
+  public getUsers() {
+    let headers = new Headers(
+    {
+      'Authorization' : this.auth.getToken()
+    });
+
+    let options = new RequestOptions({ headers: headers });
+    // Change to this http://ed43bb3b.ngrok.io/api/users
+    let url = 'http://contoh.dev/api/users';
+    this.http.get(url, options).map(res => res.json()).subscribe(
+      data => {
+        this.users = data.data;
+      }
+    );
+  }
+
+  public logout() {
+    this.auth.logout().subscribe(succ => {
+      this.nav.setRoot('LoginPage')
+    });
   }
 
 }
